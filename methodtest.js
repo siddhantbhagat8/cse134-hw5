454b730737d70ecc44edb15ff77c1501d5a24f32
siddhantbhagat <sidddhantbhagat10@gmail.com>
2023-03-13 15:05:38 -0700

    put button added

--- methodtest.js
+++ methodtest.js
@@ -39,5 +39,30 @@ export function postData() {
     };
     xhr.send();
   }
-   
+  export function putData() {
+    let id = document.getElementById("ID").value;
+    let article_name = document.getElementById("article_name").value;
+    let article_body = document.getElementById("article_body").value;
+    let date = new Date().toString();
   
+    let data = {
+      id,
+      article_name,
+      article_body,
+      date
+    };
+  
+    let url = "https://httpbin.org/put";
+  
+    let xhr = new XMLHttpRequest();
+    xhr.open("PUT", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        let response = JSON.parse(xhr.responseText);
+        let output = document.getElementById("response");
+        output.innerHTML = JSON.stringify(response, null, 2);
+      }
+    };
+    xhr.send(JSON.stringify(data));
+  }
