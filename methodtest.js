0672841641ccb03ebd9b5dcc3366e95275bc948f
siddhantbhagat <sidddhantbhagat10@gmail.com>
2023-03-14 13:10:10 -0700

    fixed methodtest.js

--- methodtest.js
+++ methodtest.js
@@ -15,7 +15,7 @@ export function postData() {
   
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Content-Type", "x-www-form-urlencoded");
     xhr.onload = function () {
       if (xhr.status === 200) {
         let response = JSON.parse(xhr.responseText);
@@ -56,7 +56,7 @@ export function postData() {
   
     let xhr = new XMLHttpRequest();
     xhr.open("PUT", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Content-Type", "x-www-form-urlencoded");
     xhr.onload = function () {
       if (xhr.status === 200) {
         let response = JSON.parse(xhr.responseText);
