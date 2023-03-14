50a1a10f04ae4254500ef0917314ce081daf1742
siddhantbhagat <sidddhantbhagat10@gmail.com>
2023-03-14 01:16:37 -0700

    style added

--- ButtonCount.js
+++ ButtonCount.js
@@ -2,14 +2,26 @@ class ButtonCount extends HTMLElement {
     constructor() {
         super();
         this.count = 0;
-        const shadow_root = this.attachShadow({ mode:"open" });
+        this.attachShadow({ mode:"open" });
         const button = document.createElement("button");
-        button.textContent = `Times Clicked: ${this.count}`;
+        button.innerHTML = `Times Clicked: ${this.count}`;
         button.addEventListener("click", () => {
             this.count++;
-            button.textContent = `Times Clicked: ${this.count}`;
+            button.innerHTML = `Times Clicked: ${this.count}`;
         });
-        shadow_root.appendChild(button);
+        this.shadowRoot.appendChild(button);
+        const style = document.createElement("style");
+        style.textContent = `
+        button {
+            background-color: #007bff;
+            border-radius: 8px;
+            cursor: pointer;
+            border: 1px solid #F5F5F5;
+            color: #F5F5F5;
+            padding: 8px 8px 8px 8px;
+        }
+        `;
+        this.shadowRoot.appendChild(style);
     }
 }
 window.customElements.define("button-count", ButtonCount);
